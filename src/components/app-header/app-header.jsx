b7fe02cd6aa4c21be67d5f2728447bad4edd3b9f
Yaroslav Shadrin <yaroslavbrok@gmail.com>
2023-06-24 13:42:15 +0300

    Сверстан компонент AppHeader

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -3,20 +3,26 @@ import { Logo, BurgerIcon, ListIcon, ProfileIcon, Button } from '@ya.praktikum/r
 
 function AppHeader() {
   return (
-    <header>
-      <Button htmlType='button' type='primary' size='medium'>
-        <BurgerIcon type='primary' />
-        <p>Конструктор</p>
-      </Button>
-      <Button htmlType='button' type='primary' size='medium'>
-        <ListIcon type='secondary' />
-        <p>Лента заказов</p>
-      </Button>
-      <Logo />
-      <Button htmlType='button' type='primary' size='medium'>
-        <ProfileIcon type='secondary' />
-        <p>Личный кабинет</p>
-      </Button>
+    <header className={styles.header}>
+      <div className={styles.wrapper}>
+        <div className={styles.orderButton}>
+          <Button htmlType='button' type='secondary' size='small' style={{ position: 'relative', zIndex: 1, color: '#F2F2F3', textAlign: 'center', alignItems: 'center', display: 'flex', gap: 8}} extraClass='pt-4 pb-4 pr-5 text text_type_main-default'>
+            <BurgerIcon type='primary' />
+            Конструктор
+          </Button>
+          <Button htmlType='button' type='secondary' size='small' style={{ position: 'relative', zIndex: 1, textAlign: 'center', alignItems: 'center', display: 'flex', gap: 8}} extraClass='pt-4 pb-4 pr-5 pl-5 text text_type_main-default text_color_inactive'>
+            <ListIcon type='secondary' />
+            Лента заказов
+          </Button>
+        </div>
+        <div className={styles.logo}>
+          <Logo />
+        </div>
+        <Button htmlType='button' type='secondary' size='small' style={{ position: 'relative', zIndex: 1, textAlign: 'center', alignItems: 'center', display: 'flex', gap: 8}} extraClass='pt-4 pb-4 pl-5 text text_type_main-default text_color_inactive'>
+          <ProfileIcon type='secondary' />
+          Личный кабинет
+        </Button>
+      </div>
     </header>
   );
 }
